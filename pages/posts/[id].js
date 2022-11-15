36762c4f1b2e4a2eef4b043a35b1bb57f88a46ed
Naveen Gullipalli <naveengullipalli317@gmail.com>
2022-11-15 16:48:58 +0100

    setup comment in post details

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,9 +5,23 @@ import ReactMarkDown from 'react-markdown'
 import'../../configureAmplify'
 //import { updatePost } from '../../src/graphql/mutations'
 import { listPosts, getPost } from '../../src/graphql/queries'
+import { creteComment } from "../../src/graphql/mutations"
+import dynamic from "next/dynamic";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"),
+    {
+    ssr:false,
+    });
+
+const intialState = {message: ""}
 
 export default function Post({ post }) {
-    const [coverImage, setCoverImage] = useState(null)
+    const [coverImage, setCoverImage] = useState(null);
+    const [comment, setComment ] = useState(intialState)
+    const [showMe, setShowMe] = useState(false)
+
+    function toggle () {
+        setShowMe(!showMe)
+    }
 
     useEffect(() => {
         updateCoverImage()
@@ -36,8 +50,22 @@ export default function Post({ post }) {
             }
             <p className='text-sm font-light my-4'> By {post.username}</p>
             <div className='mt-8'>
-            <ReactMarkDown className="prose"
+                <ReactMarkDown className="prose"
                 children={post.content}/>
+            </div>
+            <div>
+                <button type="button" className="mb-4 bg-green-600 text-white
+                font-semibold px-8 py-2 rounded-lg" onClick={toggle}>Write a Comment </button>
+
+                {
+                    <div
+                        style={{display: showMe ? "block" : "none"}}>
+                        <SimpleMDE value= ""/>
+                        <button type="button" className="mb-4 bg-green-600 text-white
+                font-semibold px-8 py-2 rounded-lg">Save</button>
+                    </div>
+
+                }
             </div>
         </div>
     )
